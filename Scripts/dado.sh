252cfbeacd20a972ee96760fe55959da06c02737
Jessy <jessica.fort.90@gmail.com>
2026-04-30 12:17:21 +0200

    Delete Scripts directory

--- Scripts/dado.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#RANDOM permite crear un número aleatorio.
-numero=$((RANDOM % 6 +1)) #Creamos una variable que genera un número aleatorio entre 6 opciones. Como empieza a contar desde 0, hay que añadirle un +1 para empiece desde el 1.
-
-echo "El resultado de la tirada es: $numero."
